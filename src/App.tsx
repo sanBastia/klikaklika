a363f73bd4733c85226044884921d38a1642c9da
San Sebastian <irsanoncode@gmail.com>
2022-07-27 11:04:24 +0700

    feat: create words array

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 	const [words, setWords] = useState(
 		'First in first out compile presenter FP void container cloud. Greedy algorithm spaghetti code cross-post gulp singleton polemical thinking observer pattern. Object library pair programming hardcoded stack developer free as speech virtual DOM service worker JSX driver. AI callback parameter injection accessibility cache senior driver. Static typing consensus ecommerce platform design pivot perf matters.'
 	);
+
+	const [wordsArray, setWordsArray] = useState<string[]>([]);
+
+	useEffect(() => {
+		setWordsArray(words.split(''));
+	}, [words]);
 
 	return (
 		<div className="flex justify-center p-6">
@@ -14,6 +20,7 @@ function App() {
 				<p className="text-4xl leading-relaxed tracking-wider">
 					{words}
 				</p>
+				<pre>{JSON.stringify(wordsArray)}</pre>
 			</div>
 		</div>
 	);
